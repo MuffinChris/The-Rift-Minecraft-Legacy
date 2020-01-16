4f2c92a0c77e6ed6fe8ae46fa6203dfde9bc1423
Deffrey <exdiark@gmail.com>
2020-01-16 08:54:21 -0800

    Stoner

--- src/com/java/rpg/classes/RPGPlayer.java
+++ src/com/java/rpg/classes/RPGPlayer.java
@@ -136,6 +136,10 @@ public class RPGPlayer extends Leveleable {
     private StatusObject bonusadS;
 
     private StatusObject autoLife;
+    private StatusObject blessing;
+    private StatusObject stoneskindr;
+    private StatusObject stoneskins;
+    private StatusObject stoneskincd;
 
 
     public StatusObject getWalkspeed() {
@@ -183,8 +187,25 @@ public class RPGPlayer extends Leveleable {
         return bonusadS;
     }
     
+    
     public StatusObject getAutoLife() {
     	return autoLife;
+    }
+    
+    public StatusObject getBlessing() {
+    	return blessing;
+    }
+    
+    public StatusObject getStoneSkinDR() {
+    	return stoneskindr;
+    }
+    
+    public StatusObject getStoneSkinS() {
+    	return stoneskins;
+    }
+    
+    public StatusObject getStoneSkinCD() {
+    	return stoneskincd;
     }
 
     public RPGPlayer(Player p) {
@@ -202,8 +223,13 @@ public class RPGPlayer extends Leveleable {
         bonusapS = new StatusObject("AP", "AP", true);
         bonusad = new StatusObject("AD", "AD", false);
         bonusadS = new StatusObject("AD", "AD", true);
-        autoLife = new StatusObject("AutoLife", "Protected", true);
-
+        
+        autoLife = new StatusObject("AutoLife", "Protected", false);
+        blessing = new StatusObject("Blessing", "Blessed", false);
+        stoneskindr = new StatusObject("Stone Skin", "", false);
+        stoneskins = new StatusObject("Stone Skin Stacks", "", false);
+        stoneskincd = new StatusObject("Stone Skin on Cooldown", "", false);
+        
         so = new ArrayList<>();
         so.add(stun);
         so.add(root);
@@ -217,7 +243,12 @@ public class RPGPlayer extends Leveleable {
         so.add(bonusad);
         so.add(bonusapS);
         so.add(bonusapS);
+        
         so.add(autoLife);
+        so.add(blessing);
+        so.add(stoneskindr);
+        so.add(stoneskins);
+        so.add(stoneskincd);
 
         player = p;
         currentMana = 0;
@@ -942,6 +973,40 @@ public class RPGPlayer extends Leveleable {
         toggles = new ArrayList<>();
         player = null;
         pclass = null;
+<<<<<<< HEAD
+=======
+        so.clear();
+        stun.scrub();
+        root.scrub();
+        silence.scrub();
+        hpfreeze.scrub();
+        manafreeze.scrub();
+        pstrength2.scrub();
+        bonusap.scrub();
+        bonusad.scrub();
+        bonusapS.scrub();
+        bonusadS.scrub();
+        autoLife.scrub();
+        blessing.scrub();
+        stoneskindr.scrub();
+        stoneskins.scrub();
+        stoneskincd.scrub();
+        bonusad = null;
+        bonusap = null;
+        bonusapS = null;
+        bonusadS = null;
+        stun = null;
+        root = null;
+        silence = null;
+        hpfreeze = null;
+        manafreeze = null;
+        pstrength2 = null;
+        autoLife = null;
+        blessing = null;
+        stoneskindr = null;
+        stoneskins = null;
+        stoneskincd = null;
+>>>>>>> parent of e784be4... Revert "StoneSkin, bishop rename"
         super.scrub();
     }
 }
