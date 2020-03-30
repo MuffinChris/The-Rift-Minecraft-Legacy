7381a8333cb3106ba98edbab6418536297b9bf8f
Deffrey <exdiark@gmail.com>
2020-03-30 14:10:13 -0700

    Add files via upload

---
+++ src/com/java/rpg/classes/skills/Earthshaker/RockToss.java
@@ -0,0 +1,86 @@
+package com.java.rpg.classes.skills.Earthshaker;
+
+import com.java.Main;
+import com.java.rpg.classes.Skill;
+import com.java.rpg.classes.StatusValue;
+import com.java.rpg.party.Party;
+import org.bukkit.*;
+import org.bukkit.entity.*;
+import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RockToss extends Skill implements Listener {
+	
+    private Main main = Main.getInstance();
+    private int damage = 20;
+    private int stunD = 20;
+    private int range = 8;
+    private int rad = 2;
+    
+    private double APscale = 3;
+    private double ADscale = 7;
+    
+    public double getDmg(Player p) {
+        return ( damage + main.getRP(p).getAP() * APscale + main.getRP(p).getAD() * ADscale ) / 5;
+    }
+    
+    public RockToss() {
+    	super("Avalanche", 100, 20, 0, 5, "%player% has shot a fireball!", "CAST-TARGET");
+    	DecimalFormat df = new DecimalFormat("#");
+        setTargetRange(range);
+    }
+    
+    public void target(Player p, LivingEntity t) {
+        super.target(p, t);
+        bigExplosion(t.getLocation());
+        makeCircle(p, t);
+    }
+    
+    public void makeCircle(Player p, LivingEntity t) {
+    	for (LivingEntity ent : p.getLocation().getNearbyLivingEntities(range)) {
+            if (ent instanceof ArmorStand) {
+                continue;
+            }
+            if (ent instanceof Player) {
+                Player pl = (Player) ent;
+                if (main.getPM().getParty(pl) instanceof Party && !main.getPM().getParty(pl).getPvp()) {
+                    if (main.getPM().getParty(pl).getPlayers().contains(p)) {
+                        continue;
+                    }
+                }
+            }
+            ent.setKiller(p);
+            spellDamage(p, ent, getDmg(p));
+            stun(p, ent, stunD);
+    	}
+    }
+    
+    public void stun(Player player, LivingEntity e, int stunDur) {
+    	if (e instanceof Player) {
+            Player pl = (Player) e;
+        	main.getRP(pl).getStun().getStatuses().add(new StatusValue("Stun:" + player.getName(), 1, stunDur, System.currentTimeMillis(), false));
+        } 
+		else {
+			e.setAI(false);
+	    	new BukkitRunnable() {
+	    		int times = 0;
+	    		public void run() {
+	    			if(times >= stunDur) {
+	    				e.setAI(true);
+	    				cancel();
+	    			}
+	    			times++;
+	    		}
+	    	}.runTaskTimer(Main.getInstance(), 0L, 1L);
+        }
+    }
+    
+    public void bigExplosion(Location loc) {
+    	loc.getWorld().spawnParticle(Particle.BLOCK_DUST, loc, 40, 2, 2, 2);
+    }
+    
+}
---
+++ src/com/java/rpg/classes/skills/Earthshaker/supers/UnstopForce.java
@@ -0,0 +1,60 @@
+package com.java.rpg.classes.skills.Earthshaker.supers;
+
+import com.java.Main;
+import com.java.rpg.classes.Skill;
+import com.java.rpg.classes.StatusValue;
+import com.java.rpg.party.Party;
+import org.bukkit.*;
+import org.bukkit.entity.*;
+import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnstopForce extends Skill implements Listener {
+	
+    private Main main = Main.getInstance();
+    private double speed = .2;
+    
+    private double APscale = .5;
+    private double ADscale = 1;
+    
+    public double getDmg(Player p) {
+        return ( damage + main.getRP(p).getAP() * APscale + main.getRP(p).getAD() * ADscale );
+    }
+    
+    public UnstopForce() {
+    	super("Unstoppable Force", 100, 20, 0, 5, "%player% has shot a fireball!", "CAST");
+    	DecimalFormat df = new DecimalFormat("#");
+        List<String> desc = new ArrayList<>();
+        setDescription(desc);
+    }
+    
+    public void cast(Player p) {
+    	super.cast(p);
+    	
+    }
+    
+    public boolean detectCollision(Player p) {
+    	for(LivingEntity ent: p.getLocation().getNearbyLivingEntities(detectRadius)) {
+    		return true;
+    	}
+    	return false;
+    }
+    
+    public void charge(Player p) {
+    	Location start = p.getLocation().clone();
+    	Vector face = p.getEyeLocation().getDirection().clone();
+    	p.teleport(start.add(face).multiply(speed));
+    }
+    
+    public void collide(Player p) {
+    	for(LivingEntity ent: p.getNearbyLivingEntities(hitRad)) {
+    		ent.setVelocity(new Vector(0,launchVelocity,0));
+    		spellDamage(p,ent,getDmg(p));
+    	}
+    }
+}
