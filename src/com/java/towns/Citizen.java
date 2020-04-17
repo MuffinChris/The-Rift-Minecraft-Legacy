280e2895fe78e99fb158b2e5c0d1fdbbaa29c2f5
Michael Chen <mdchenmail@gmail.com>
2020-04-16 17:19:16 -0700

    citizen data storage

--- src/com/java/towns/Citizen.java
+++ src/com/java/towns/Citizen.java
@@ -2,7 +2,9 @@ package com.java.towns;
 
 import com.java.Main;
 import org.bukkit.entity.Player;
-
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,13 +14,16 @@ public class Citizen {
     private String town; //Player's town, current "Randos" if townless
 
     public Citizen(Player pl){
-        this.p = pl; this.r = -1; this.town = "Randos";
+        this.p = pl; this.r = -1; this.town = "none";
+        pullFiles();
     }
     public Citizen(Player pl, int rnk){
         this.p = pl; this.r = rnk;
+        pullFiles();
     }
     public Citizen(Player pl, int rnk, String t){
         this.p = pl; this.r = rnk; this.town = t;
+        pullFiles();
     }
 
     public Player getPlayer(){
@@ -40,4 +45,60 @@ public class Citizen {
     public void kick(Player recieve){
 
     }
+
+
+
+    public void setUsernameFile(FileConfiguration pData, String username) {
+        if (pData.contains("Username")) {
+            if (pData.getString("Username").equalsIgnoreCase(p.getName())) {
+                pData.set("Username", username);
+            } else {
+                pData.set("PreviousUsername", pData.getString("Username"));
+                pData.set("Username", username);
+            }
+        } else {
+            pData.set("Username", username);
+        }
+    }
+    public void setLastSeenFile(FileConfiguration pData, Long millis) {
+        pData.set("LastSeen", millis);
+    }
+    public void pushFiles(){
+        File pFile = new File("plugins/Rift/data/towns/" + p.getUniqueId() + ".yml");
+        FileConfiguration pData = YamlConfiguration.loadConfiguration(pFile);
+        try{
+             /*
+            Utility
+             */
+            setUsernameFile(pData, p.getName());
+            setLastSeenFile(pData, System.currentTimeMillis());
+            /*
+            Town info
+             */
+            pData.set("CurrentTown", town);
+            pData.set("Rank", r);
+
+            pData.save(pFile);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void pullFiles(){
+        File pFile = new File("plugins/Rift/data/towns/" + p.getUniqueId() + ".yml");
+        FileConfiguration pData = YamlConfiguration.loadConfiguration(pFile);
+        if(!pFile.exists()){
+            pushFiles(); pullFiles();
+        }
+        else{
+            setUsernameFile(pData, p.getName());
+            setLastSeenFile(pData, System.currentTimeMillis());
+            /*
+            Town info
+             */
+            setRank(pData.getInt("Rank"));
+            setTown(pData.getString("CurrentTown"));
+        }
+    }
+
 }
